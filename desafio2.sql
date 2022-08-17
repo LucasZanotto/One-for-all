abc1322c2b275a60d44c480019974fdc09d325f8
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2022-08-17 17:40:50 -0300

    req 2

---
+++ desafio2.sql
@@ -0,0 +1,6 @@
+SELECT COUNT(ca.cancoes) cancoes, COUNT(ar.name) artistas, COUNT(al.album) albuns
+FROM cancoes ca
+JOIN album al
+ON ca.album_id = al.album_id
+JOIN artist ar
+ON al.artista_id = ar.artist_id;
