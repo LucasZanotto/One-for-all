1cca89fe3df5317fd894a1f9cbf8059e42ecac09
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2022-08-17 18:38:25 -0300

    req 3 att02

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,6 @@
-USE SpotifyClone
 SELECT COUNT(ca.cancoes) cancoes, COUNT(ar.name) artistas, COUNT(al.album) albuns
-FROM cancoes ca
-JOIN album al
+FROM SpotifyClone.cancoes ca
+JOIN SpotifyClone.album al
 ON ca.album_id = al.album_id
-JOIN artist ar
+JOIN SpotifyClone.artist ar
 ON al.artista_id = ar.artist_id;
--- desafio3.sql
+++ desafio3.sql
@@ -1,8 +1,8 @@
-SELECT SpotifyClone.us.usuario usuario, COUNT(SpotifyClone.hi.usuario_id) qt_de_musicas_ouvidas, ROUND((SUM(SpotifyClone.ca.duracao_segundos) / 60), 2) total_minutos
+SELECT us.usuario usuario, COUNT(hi.usuario_id) qt_de_musicas_ouvidas, ROUND((SUM(ca.duracao_segundos) / 60), 2) total_minutos
 FROM SpotifyClone.usuario us
 JOIN SpotifyClone.historico_de_reproducoes hi
-ON SpotifyClone.us.usuario_id = SpotifyClone.hi.usuario_id
+ON us.usuario_id = hi.usuario_id
 JOIN SpotifyClone.cancoes ca
-ON SpotifyClone.ca.cancoes_id = SpotifyClone.hi.cancoes_id
-GROUP BY SpotifyClone.hi.usuario_id
-ORDER BY SpotifyClone.us.usuario ASC;
+ON ca.cancoes_id = hi.cancoes_id
+GROUP BY hi.usuario_id
+ORDER BY us.usuario ASC;
