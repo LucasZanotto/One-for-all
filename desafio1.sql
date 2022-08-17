14581eb5b4bf998deffb27ffc819c5f1a128b6b5
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2022-08-17 17:25:43 -0300

    req 1 finalizado

--- desafio1.sql
+++ desafio1.sql
@@ -4,8 +4,8 @@ CREATE SCHEMA SpotifyClone;
 USE SpotifyClone;
 
 CREATE TABLE SpotifyClone.artist(
-artist_id INT PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(100)
+artist_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+name VARCHAR(100) NOT NULL
 );
 
 INSERT INTO SpotifyClone.`artist`(name)
@@ -16,67 +16,12 @@ VALUES ('Beyoncé'),
 ('Blind Guardian'),
 ('Nina Simone');
 
-
-CREATE TABLE SpotifyClone.cancoes(
-cancoes_id INT PRIMARY KEY AUTO_INCREMENT,
-cancoes VARCHAR(100),
-duracao_segundos INT
-);
-
-INSERT INTO SpotifyClone.`cancoes`(cancoes, duracao_segundos)
-VALUES ('BREAK MY SOUL',279),
-('VIRGO’S GROOVE', 369),
-('ALIEN SUPERSTAR', 116),
-('Don’t Stop Me Now', 203),
-('Under Pressure', 152),
-('Como Nossos Pais', 105),
-('O Medo de Amar é o Medo de Ser Livre', 207),
-('Samba em Paris', 267),
-('The Bard’s Song', 244),
-('Feeling Good', 100);
-
-CREATE TABLE SpotifyClone.usuario(
-usuario_id INT PRIMARY KEY AUTO_INCREMENT,
-usuario VARCHAR(100),
-idade INT
-);
-
-INSERT INTO SpotifyClone.`usuario`(usuario, idade)
-VALUES ('Barbara Liskov', 82),
-('Robert Cecil Martin', 58),
-('Ada Lovelace', 37),
-('Martin Fowler', 46),
-('Sandi Metz', 58),
-('Paulo Freire', 19),
-('Bell Hooks', 26),
-('Christopher Alexander', 85),
-('Judith Butler', 45),
-('Jorge Amado', 58);
-
-CREATE TABLE SpotifyClone.plano(
-plano_id INT PRIMARY KEY AUTO_INCREMENT,
-plano VARCHAR(100),
-data_assinatura VARCHAR(100),
-valor_plano VARCHAR(100)
-);
-
-INSERT INTO SpotifyClone.`plano`(plano, data_assinatura, valor_plano)
-VALUES ('gratuito', '2019-10-20', '0.00'),
-('gratuito', '2017-01-06', '0.00'),
-('familiar', '2017-12-30', '7.99'),
-('familiar', '2017-01-17', '7.99'),
-('familiar', '2018-04-29', '7.99'),
-('universitário', '2018-02-14', '5.99'),
-('universitário', '2018-01-05', '5.99'),
-('pessoal', '2019-06-05', '6.99'),
-('pessoal', '2020-05-13', '6.99'),
-('pessoal', '2017-02-17', '6.99');
-
 CREATE TABLE SpotifyClone.album(
-album_id INT PRIMARY KEY AUTO_INCREMENT,
-album VARCHAR(100),
-artista_id INT,
-ano_lancamento INT
+album_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+album VARCHAR(100) NOT NULL,
+artista_id INT NOT NULL,
+ano_lancamento INT NOT NULL,
+FOREIGN KEY(artista_id) REFERENCES artist(artist_id)
 );
 
 INSERT INTO SpotifyClone.`album`(album, artista_id, ano_lancamento)
@@ -89,11 +34,66 @@ VALUES ('Renaissance', 1, 2022),
 ('Somewhere Far Beyond?', 5, 2007),
 ('I Put A Spell On You', 6, 2012);
 
+CREATE TABLE SpotifyClone.cancoes(
+cancoes_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+cancoes VARCHAR(100) NOT NULL,
+duracao_segundos INT NOT NULL,
+album_id INT NOT NULL,
+FOREIGN KEY (album_id) REFERENCES SpotifyClone.album(album_id)
+);
+
+INSERT INTO SpotifyClone.`cancoes`(cancoes, duracao_segundos, album_id)
+VALUES ('BREAK MY SOUL',279, 1),
+('VIRGO’S GROOVE', 369, 1),
+('ALIEN SUPERSTAR', 116, 1),
+('Don’t Stop Me Now', 203, 2),
+('Under Pressure', 152, 3),
+('Como Nossos Pais', 105, 4),
+('O Medo de Amar é o Medo de Ser Livre', 207, 5),
+('Samba em Paris', 267, 6),
+('The Bard’s Song', 244, 7),
+('Feeling Good', 100, 8);
+
+CREATE TABLE SpotifyClone.plano(
+plano_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+escolha_id VARCHAR(100) NOT NULL,
+valor_plano INT NOT NULL
+);
+
+INSERT INTO SpotifyClone.`plano`(escolha_id, valor_plano)
+VALUES ('gratuito', '0.00'),
+('familiar', '7.99'),
+('universitário', '5.99'),
+('pessoal', '6.99');
+
+CREATE TABLE SpotifyClone.usuario(
+usuario_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+usuario VARCHAR(100) NOT NULL,
+idade INT NOT NULL,
+data_assinatura VARCHAR(100) NOT NULL,
+plano_id INT NOT NULL,
+FOREIGN KEY (plano_id) REFERENCES SpotifyClone.plano(plano_id)
+);
+
+INSERT INTO SpotifyClone.`usuario`(usuario, idade, data_assinatura, plano_id)
+VALUES ('Barbara Liskov', 82, '2019-10-20', 1),
+('Robert Cecil Martin', 58, '2017-01-06', 1),
+('Ada Lovelace', 37, '2017-12-30', 2),
+('Martin Fowler', 46, '2017-01-17', 2),
+('Sandi Metz', 58, '2018-04-29', 2),
+('Paulo Freire', 19, '2018-02-14', 3),
+('Bell Hooks', 26, '2018-01-05', 3),
+('Christopher Alexander', 85, '2019-06-05', 4),
+('Judith Butler', 45, '2020-05-13', 4),
+('Jorge Amado', 58, '2017-02-17', 4);
+
 CREATE TABLE SpotifyClone.historico_de_reproducoes(
-usuario_id INT,
-cancoes_id INT,
-data_reproducao VARCHAR(100),
-PRIMARY KEY(usuario_id, cancoes_id)
+usuario_id INT NOT NULL,
+cancoes_id INT NOT NULL,
+data_reproducao DATETIME NOT NULL,
+PRIMARY KEY(usuario_id, cancoes_id),
+FOREIGN KEY (usuario_id) REFERENCES SpotifyClone.usuario(usuario_id),
+FOREIGN KEY (cancoes_id) REFERENCES SpotifyClone.cancoes(cancoes_id)
 );
 
 INSERT INTO SpotifyClone.`historico_de_reproducoes`(usuario_id, cancoes_id, data_reproducao)
@@ -115,9 +115,11 @@ VALUES (1, 8, '2022-02-28 10:45:55'),
 (10, 8, '2015-12-13 08:30:22');
 
 CREATE TABLE SpotifyClone.cancoes_album(
-cancoes_id INT AUTO_INCREMENT,
-album_id INT,
-PRIMARY KEY(cancoes_id, album_id)
+cancoes_id INT AUTO_INCREMENT NOT NULL,
+album_id INT NOT NULL,
+PRIMARY KEY(cancoes_id, album_id),
+FOREIGN KEY (cancoes_id) REFERENCES SpotifyClone.cancoes(cancoes_id),
+FOREIGN KEY (album_id) REFERENCES SpotifyClone.album(album_id)
 );
 
 INSERT INTO SpotifyClone.`cancoes_album`(album_id)
@@ -133,9 +135,11 @@ VALUES (1),
 (8);
 
 CREATE TABLE SpotifyClone.seguindo_artistas(
-usuario_id INT,
-artist_id INT,
-PRIMARY KEY(usuario_id, artist_id)
+usuario_id INT NOT NULL,
+artist_id INT NOT NULL,
+PRIMARY KEY(usuario_id, artist_id),
+FOREIGN KEY (usuario_id) REFERENCES SpotifyClone.usuario(usuario_id),
+FOREIGN KEY (artist_id) REFERENCES SpotifyClone.artist(artist_id)
 );
 
 INSERT INTO SpotifyClone.`seguindo_artistas`(usuario_id, artist_id)
@@ -151,6 +155,5 @@ VALUES (1, 1),
 (6, 6),
 (6, 1),
 (7, 6),
-(8, 0),
 (9, 3),
 (10, 2);
