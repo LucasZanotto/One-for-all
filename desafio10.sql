2bb24d137c927340ca739e9778bafa8e1131e589
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2022-08-18 14:44:04 -0300

    req 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,8 +1,9 @@
-SELECT ca.cancoes nome, COUNT(hi.usuario_id) reproducoes
+SELECT ca.cancoes nome, COUNT(hi.cancoes_id) reproducoes
 FROM cancoes ca
-JOIN album al
-ON ca.album_id = al.album_id
 JOIN historico_de_reproducoes hi
 ON ca.cancoes_id = hi.cancoes_id
+JOIN usuario us
+ON us.usuario_id = hi.usuario_id
+WHERE us.plano_id = 1 OR us.plano_id = 4
 GROUP BY ca.cancoes
 ORDER BY ca.cancoes ASC;
