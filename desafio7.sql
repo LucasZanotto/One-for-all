78113bfdac0bf7ab8242d3d14e06ba147c51fcc2
Lucas Abati Zanotto <lucasabatizanotto@gmail.com>
2022-08-18 15:23:11 -0300

    req 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,8 @@
-SELECT ar.name artista, al.album album, se.usuario_id seguidores
+SELECT ar.name AS artista, al.album AS album, COUNT(se.usuario_id) seguidores
 FROM artist ar
 JOIN album al
 ON al.artista_id = ar.artist_id
 JOIN seguindo_artistas se
-ON se.artist_id = ar.artist_id;
+ON se.artist_id = ar.artist_id
+GROUP BY al.album, ar.name
+ORDER BY COUNT(se.usuario_id) DESC, ar.name ASC;
